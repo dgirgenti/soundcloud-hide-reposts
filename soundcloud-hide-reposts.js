f648d9f758341f74eb806d383f5d69bddb87b4bf
Matt <github@dopelogik.com>
2016-01-07 18:53:23 -0500

    Updated for new SoundCloud UI, removed jQuery dependency.

--- soundcloud-hide-reposts.js
+++ soundcloud-hide-reposts.js
@@ -1,23 +1,8 @@
 (function() {
-    var v = "1.11.1";
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "//ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-        script.onload = script.onreadystatechange = function() {
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                initHideReposts();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
-        initHideReposts();
-    }
-
-    function initHideReposts() {
-        (window.HideReposts = function() {
-            $(".soundList__item").has(".soundTitle__info").hide();
-        })();
-    }
+	var ps = document.querySelectorAll(".soundList__item"); 
+	for (var i=0; i<ps.length; i++){ 
+		var p = ps[i]; 
+		if (p.innerHTML.indexOf('reposted')>-1) 
+			p.remove(); 
+	}
 })();
